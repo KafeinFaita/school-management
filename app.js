cd9c556699a0d0439a41c5a6c6b4dbd965849317
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2022-02-26 19:17:04 +0800

    heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser')
 const mainRoute = require('./routes/mainRoute')
 const authRoute = require('./routes/authRoute')
 
-const port = 3001 || process.env.PORT;
+const port = process.env.PORT || 3001;
 
 const dbURI = "mongodb+srv://kafein:kafeinfaita@cluster0.3xefo.mongodb.net/school?retryWrites=true&w=majority"
 
