d8f2ce96c781c93dae676f24949c66ead314d3d5
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2022-03-01 21:16:41 +0800

    fixed middleware

--- controllers/middleware.js
+++ controllers/middleware.js
@@ -1,42 +1,52 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
-module.exports.auth_user = async (req, res) => {
-    const token = req.cookies.jwt
-    jwt.verify(token, 'schooldb secret', (err, decodedToken) => {
-        if (err) {
-            console.log(err.message)
-            res.json({ verified: false })
-        } else {
+module.exports.auth_user = (goToNext) => {
+    // const token = req.cookies.jwt
+    // jwt.verify(token, 'schooldb secret', (err, decodedToken) => {
+    //     if (err) {
+    //         console.log(err.message)
+    //         res.json({ verified: false })
+    //     } 
+    //     next()
+    // })
+    return (req, res, next) => {
+        const token = req.cookies.jwt
+        jwt.verify(token, 'schooldb secret', (err, decodedToken) => {
+            if (err) {
+                console.log(err.message)
+                return res.json({ verified: false, msg: "Please log in." })
+            } 
+            
+            if (goToNext) {
+                return next()
+            }
             res.json({ verified: true })
-        }
-    })
+        })
+    }
 }
 
 module.exports.auth_user_role = (role) => {
     return (req, res) => {
         const token = req.cookies.jwt
 
-        if (token) {
-            jwt.verify(token, 'schooldb secret', async (err, decodedToken) => {
-                
-                if (err) {
-                    res.json({ verified: false })
-                } else {
-                    let user = await User.findById(decodedToken.id)
+        jwt.verify(token, 'schooldb secret', async (err, decodedToken) => {
+            
+            if (err) {
+                res.json({ verified: false, msg: "Unknown error." })
+            } else {
+                let user = await User.findById(decodedToken.id)
 
-                    // const userHasAccess = role.some(r => user.roles.includes(r)) <-- for multiple roles
-                    
-                    if (role.includes(user.role)) {
-                        res.json({ verified: true })
-                    } else {
-                        res.json({ verified: false, msg: "You don't have enough privilege to view this page."  })
-                    }
-                    
+                // const userHasAccess = role.some(r => user.roles.includes(r)) <-- for multiple roles
+                
+                if (role.includes(user.role)) {
+                    res.json({ verified: true, authenticated: true })
+                } else {
+                    res.json({ verified: true, authenticated: false, msg: "You don't have enough privilege to view this page."  })
                 }
-            })
-        } else {
-            res.json({ verified: false, msg: "Please log in." })
-        }
+                
+            }
+        })
+         
     }
 }
